447d8b77011c059c87cf4324458206c59894398d
orlandobel <orlando.bel.jimmy@gmail.com>
2025-07-01 09:31:10 -0600

    fix case senssitive file names

--- src/components/moviecard/mooviecard.tsx
+++ src/components/moviecard/mooviecard.tsx
@@ -3,7 +3,7 @@ import noPoster from "@assets/img/no-poster.png"
 export const MovieCard = () => {
     return (
         <div className="bg-white flex flex-col rounded-[0.5rem]">
-            <img className="mx-4 aspect-[2/3] h-[35svh]" src={noPoster} alt="" />
+            <img className="mx-4 aspect-[1.85/2.35] h-[35svh]" src={noPoster} alt="" />
             <div className="bg-sky-700 text-white flex flex-col rounded-b-[0.5rem] gap-1 px-4 py-1">
                 <label className="flex justify-between">
                     <span className="font-bold">Some Large Movie Name</span>
