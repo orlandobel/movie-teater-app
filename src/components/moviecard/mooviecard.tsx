bfbb451c0d08839fa782ac876739591b4d6ec936
orlandobel <orlando.bel.jimmy@gmail.com>
2025-07-01 12:09:13 -0600

    Movie card component

--- src/components/moviecard/mooviecard.tsx
+++ src/components/moviecard/mooviecard.tsx
@@ -2,15 +2,17 @@ import noPoster from "@assets/img/no-poster.png"
 
 export const MovieCard = () => {
     return (
-        <div className="bg-white flex flex-col rounded-[0.5rem]">
-            <img className="mx-4 aspect-[1.85/2.35] h-[35svh]" src={noPoster} alt="" />
-            <div className="bg-sky-700 text-white flex flex-col rounded-b-[0.5rem] gap-1 px-4 py-1">
+        <div className="bg-gray-300 flex flex-col rounded-[0.5rem]">
+            <div className="h-[45svh] mx-4">
+                <img className="aspect-[1.85/2.35]" src={noPoster} alt="" />
+            </div>
+            <div className="bg-blue-800 text-white flex flex-col rounded-b-[0.5rem] gap-1 px-4 py-1">
                 <label className="flex justify-between">
                     <span className="font-bold">Some Large Movie Name</span>
                     <small>120min</small>
                     <small>A</small>
                 </label>
-                <a className="">Comprar boletos →</a>
+                <a className="underline text-cyan-200">Comprar boletos →</a>
             </div>
         </div>
     )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,11 +5,11 @@ export const HomePage = () => {
     return <>
         <Mainlayout>
             <MovieCard />
-            <div className="border rounded-[8px] p-2">div 2</div>
-            <div className="border rounded-[8px] p-2">div 3</div>
-            <div className="border rounded-[8px] p-2">div 4</div>
-            <div className="border rounded-[8px] p-2">div 5</div>
-            <div className="border rounded-[8px] p-2">div 6</div>
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
         </Mainlayout>
     </>
 }
