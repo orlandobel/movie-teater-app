4d0f567f11f90aff3f8bff32ec8de40b76356363
orlandobel <orlando.bel.jimmy@gmail.com>
2025-07-08 05:47:34 -0600

    Validate click on occupied seat

--- src/components/seats/Seat.tsx
+++ src/components/seats/Seat.tsx
@@ -10,7 +10,12 @@ interface SeatProps {
 export const Seat = ({ index, seat, onClickSeat }: SeatProps) => {
     const className = `SeatsRow__seat ${seat.selected ? "SeatsRow__seat--selected" : ""} ${seat.occupied ? "SeatsRow__seat--occupied" : ""}`
 
+    function onClick() {
+        if (seat.occupied) return
+        onClickSeat(index)
+    }
+
     return (
-        <span className={className} onClick={() => onClickSeat(index)}>{seat.number}</span>
+        <span className={className} onClick={() => onClick()}>{seat.number}</span>
     )
 }
