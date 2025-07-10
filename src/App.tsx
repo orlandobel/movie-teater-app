00726f690ff3e5a072f2d03666b3eef841f35f54
orlandobel <orlando.bel.jimmy@gmail.com>
2025-07-10 06:03:36 -0600

    add store to main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import '@/App.css'
 import { AppRouter } from '@/routes/AppRouter'
+import { useMovieStore } from '@/stores/moviesstore'
+import { useEffect } from 'react'
+import { staticMovies } from '@/stores/moviesstore'
 
 function App() {
+    const setMovies = useMovieStore((state) => state.setMovies)
+    
+    useEffect(() => {
+        setMovies(staticMovies())
+    }, [])
+    
     return (<>
             <AppRouter />
         </>)
