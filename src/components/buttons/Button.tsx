a71fbba1e58a6262ec35fe8936c60ce0a334c546
orlandobel <orlando.bel.jimmy@gmail.com>
2025-07-10 05:17:20 -0600

    New movie page

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -3,9 +3,10 @@ import "./buttons.css"
 interface ButtonProps {
     children: React.ReactNode
     buttonType?: "primary" | "secondary" | "warning" | "danger"
+    className?: string
     onClick?: () => void
 }
 
-export const Button = ({ children, buttonType = "primary", onClick }: ButtonProps) => {
-    return (<button className={`btn btn--${buttonType}`} onClick={onClick}>{children}</button>)
+export const Button = ({ children, buttonType = "primary", className, onClick }: ButtonProps) => {
+    return (<button className={`btn btn--${buttonType} ${className}`} onClick={onClick}>{children}</button>)
 }
